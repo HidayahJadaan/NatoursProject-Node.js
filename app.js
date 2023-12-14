89fb07d466c09c8744c189ae5ea0a66c1d8961ba
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-14 19:29:40 +0300

    Serving Static Files

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const  express = require('express');
 const morgan = require('morgan'); //3rd-party middleware
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
-
 // 2. Creating Variable Called app --> Calling express function which holds many methods that we can use it in our app
 const app = express();
-
 // CREATE SIMPLE MIDDLEWARE --> it is just a function that can modify the incomming request data (middleware--> in the middle of REQUEST & RESPONSE)
-
 app.use(morgan('dev'));
-
 app.use(express.json());
+// SERVING STATIC FILES ==> localhost:port/overview.html
+app.use(express.static(`${__dirname}/public`))
+
+
+
+
+
 // CREATE OUR OWN MIDDLEWARE
 app.use((re, res, next) => {
   console.log('Hello from the middleware');
