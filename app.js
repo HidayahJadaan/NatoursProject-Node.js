1b0c76c6da2e19b2321e848ee90e1e8c18361904
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-14 21:01:35 +0300

    Environment Variables

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // 1. Requiring THe Modeule
-const  express = require('express');
+const express = require('express');
 const morgan = require('morgan'); //3rd-party middleware
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 // 2. Creating Variable Called app --> Calling express function which holds many methods that we can use it in our app
 const app = express();
-// CREATE SIMPLE MIDDLEWARE --> it is just a function that can modify the incomming request data (middleware--> in the middle of REQUEST & RESPONSE)
-app.use(morgan('dev'));
+
+// READING ENV VARIABLES
+if (process.env.NODE_ENV !== 'development') {
+  // CREATE SIMPLE MIDDLEWARE --> it is just a function that can modify the incomming request data (middleware--> in the middle of REQUEST & RESPONSE)
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 // SERVING STATIC FILES ==> localhost:port/overview.html
-app.use(express.static(`${__dirname}/public`))
-
-
-
-
+app.use(express.static(`${__dirname}/public`));
 
 // CREATE OUR OWN MIDDLEWARE
 app.use((re, res, next) => {
@@ -27,12 +28,11 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // ROUTES
 // Mountain The Router
-app.use('/api/v1/tours', tourRouter)// use this middleware to a specific route
+app.use('/api/v1/tours', tourRouter); // use this middleware to a specific route
 // =================== USERS =========================
-app.use('/api/v1/users', userRouter)// use this middleware to a specific route
+app.use('/api/v1/users', userRouter); // use this middleware to a specific route
 
 // ==================================================
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+// In order to read config.env file and dfeine the varaiables in our node app
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+// console.log(process.env);
+
 const app = require('./app');
 
+// Environmetn Variable: Gloabal Variable that is used to define environmet which a node app is running(set by express)
+console.log(app.get('env'));
+// IN NODE (many environment variables(comes from process core module))
+
+
+// TO START DEFINING ENVIRONMENT VARIABLE --> NODE_ENV=development nodemon server.js
+
+const port = process.env.PORT || 7000;
 // START THE SERVER
 // THIS ALWAYS THE LAST STEP
 // 3. Listening for The Server
-app.listen(3000, () => {
-    // This function is called when the server is listening on the port
-    console.log('App listening on port 3000 ...');
-  });
-  
+app.listen(port, () => {
+  // This function is called when the server is listening on the port
+  console.log('App listening on port 3000 ...');
+});
