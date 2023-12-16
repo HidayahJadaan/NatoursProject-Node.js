cccc6657b46c7f962391f12672ba502292086895
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-16 19:27:04 +0300

    Creating Simple Data Model

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
+const mongoose = require('mongoose');
+
 // In order to read config.env file and dfeine the varaiables in our node app
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
-
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    // mongoose.connect(DB, { // HOSTED db vERSION
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((conn) => {
+    // console.log(conn.connections);
+    console.log('Connected to the database');
+  });
 
 // Environmetn Variable: Gloabal Variable that is used to define environmet which a node app is running(set by express)
-console.log(app.get('env'));
+// console.log(app.get('env'));
 // IN NODE (many environment variables(comes from process core module))
 
+// CREATING SCHEMA (DESCRIPTION)
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, ' A tour must have a price'],
+  },
+});
+// CREATING A MODEL
+const Tour = mongoose.model('Tour', toursSchema);
 
 // TO START DEFINING ENVIRONMENT VARIABLE --> NODE_ENV=development nodemon server.js
-
 const port = process.env.PORT || 7000;
 // START THE SERVER
 // THIS ALWAYS THE LAST STEP
